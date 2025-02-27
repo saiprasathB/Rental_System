a358063c2da05445b82d98649892083d030c1fcb
saiprasathB <saiprasath2805@gmail.com>
2025-02-27 09:12:48 +0530

    Error fixed auth/api to /

--- rental_system_backend/server.js
+++ rental_system_backend/server.js
@@ -14,7 +14,8 @@ app.use(cors());
 app.use(express.json());
 
 
-app.use("/api/auth", authRoutes);
+app.use("/", authRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
